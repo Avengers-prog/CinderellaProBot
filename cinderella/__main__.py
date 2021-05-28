3107f393287c23baa2e33933335360f7c0a009b0
Sooraj <76552813+Avengers-prog@users.noreply.github.com>
2021-05-28 12:48:58 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -27,16 +27,17 @@ from cinderella.modules.connection import connect_button
 
 
 PM_START_TEXT = """
-Hello {}
-My name is {}\n𝗔 𝗣𝗼𝘄𝗲𝗿𝗳𝘂𝗹 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗣𝗿𝗼𝗕𝗼𝘁 𝘁𝗼 𝗠𝗮𝗻𝗮𝗴𝗲 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽𝘀,𝗳𝗲𝗲𝗹 𝗳𝗿𝗲𝗲 𝘁𝗼 𝗮𝗱𝗱 𝘁𝗼 𝘆𝗼𝘂𝗿 𝗴𝗿𝗼𝘂𝗽𝘀!!
-Maintained by  This is Bot Of @Team_Avengerz
+𝐇𝐞𝐥𝐥𝐨 {}
+Mʏ ɴᴀᴍᴇ ɪs {}\n𝗔 𝗣𝗼𝘄𝗲𝗿𝗳𝘂𝗹 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗣𝗿𝗼𝗕𝗼𝘁 𝘁𝗼 𝗠𝗮𝗻𝗮𝗴𝗲 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽𝘀,𝗳𝗲𝗲𝗹 𝗳𝗿𝗲𝗲 𝘁𝗼 𝗮𝗱𝗱 𝘁𝗼 𝘆𝗼𝘂𝗿 𝗴𝗿𝗼𝘂𝗽𝘀!!
+𝗠𝗮𝗶𝗻𝘁𝗮𝗶𝗻𝗲𝗱 𝗕𝘆 @Team_Avengerz
+𝗠𝗮𝗱𝗲 𝘄𝗶𝘁𝗵 ❤️ 𝗯𝘆 @Rohithaditya 𝗮𝗻𝗱 @HackingburnSJ
 """
 
 
 HELP_STRINGS = """
 Hey there! My name is {}.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
-the things I can help you with. 𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗕𝘆 @HackingburnSJ 𝗮𝗻𝗱 @BusyInHell
+the things I can help you with.
 This is Bot Of @Team_Avengerz
 *Main* commands available:
  💠 - /start: start the bot
