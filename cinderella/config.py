2ea6d04fcf2f398e6226ecd490db4f13afa5ac8f
Sooraj <76552813+Avengers-prog@users.noreply.github.com>
2021-05-31 12:52:07 +0530

    Done -- @Rohithaditya

--- cinderella/config.py
+++ cinderella/config.py
@@ -5,6 +5,35 @@ import os
 def get_user_list(config, key):
     with open('{}/cinderella/{}'.format(os.getcwd(), config), 'r') as json_file:
         return json.load(json_file)[key]
+    
+def get_str_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.str(name, default=default)) and not required:
+        log.warn("No str key: " + name)
+        return None
+    elif not data:
+        log.critical("No str key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
+
+def get_int_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.int(name, default=default)) and not required:
+        log.warn("No int key: " + name)
+        return None
+    elif not data:
+        log.critical("No int key: " + name)
+        sys.exit(2)
+    else:
+        return data
 
 
 # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
